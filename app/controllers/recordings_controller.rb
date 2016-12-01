bcf750f3b575acd5db280d12a64dde97fb741557
Johan Brissmyr <brissmyr@gmail.com>
2016-11-30 21:59:21 -0800

    Don't overwrite when not done

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -2,10 +2,20 @@ class RecordingsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def record
-    r = Recording.where(cookie: params['interaction']['cookieId']).first_or_initialize
-    r.user_agent = params['interaction']['userAgent']
-    r.stats      = params['interaction']['stats'].to_json
-    r.save!
+    r = Recording.find_by(cookie: params['interaction']['cookieId'])
+
+    if r.nil?
+      r = Recording.new
+      r.cookie = params['interaction']['cookieId']
+      r.user_agent = params['interaction']['userAgent']
+      r.stats = params['interaction']['stats'].to_json
+      r.save!
+    end
+
+    if params['interaction']['stats']['done'] == 'true'
+      r.stats = params['interaction']['stats'].to_json
+      r.save!
+    end
 
     head :ok
   end
